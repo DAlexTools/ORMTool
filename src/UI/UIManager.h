3270d95dcc77f342d3ac553d2c266b170853cb32
DAlexTools <dalexandrovtools@gmail.com>
2026-01-10 19:36:19 +0300

    update project
    - modified CMakeLists.txt
    - deleted dev_ops/.gitignore file
    - modified src/UI/UIManager.h

--- src/UI/UIManager.h
+++ src/UI/UIManager.h
@@ -1,11 +1,10 @@
-#pragma once 
-#include <string>
+#pragma once
 #include <atomic>
 #include <future>
 #include <thread>
 #include <mutex>
 #include <functional>
-#include <GLFW/glfw3.h>
+
 
 #include <array>
 #include <cmath>
@@ -15,51 +14,9 @@
 
 #include "ImNeo.h"
 #include <map>
+#include "Utils/Types.h"
 
-/** enum channels  */
-enum class ORMChannel : int
-{ 
-	AllRGB, 
-	AO_R, 
-	Roughness_G, 
-	Metallic_B 
-};
-
-/** preview texture data */
-struct alignas(32) PreviewTexture
-{
-	unsigned char* data = nullptr;
-	std::string path;
-	int width = 0;
-	int height = 0;			//
-	GLuint glId = 0;		// 4 byte
-	GLuint channelR = 0; 	// 4 byte
-	GLuint channelG = 0; 	// 4 byte
-	GLuint channelB = 0; 	// 4 byte
-
-	/** loading texture */
-	bool Load(const std::string& p);
-
-	/** unloading texture */
-	void Unload();
-
-	/** generate texture */
-	void GenerateChannelsFromRGB(unsigned char* src, int w, int h);
-};
-
-struct SaveData
-{
-	std::string ao;
-	std::string rough;
-	std::string metal;
-	std::string saveUnrealPath;
-	std::string saveUnityPath;
-
-	std::function<void(float*)> progressCallback;
-
-};
-
-class UIManager final 
+class UIManager final
 {
 public:
 	UIManager();
@@ -93,17 +50,17 @@ private:
 	int roughResolutionIndex = 0;
 	int metalResolutionIndex = 0;
 
-	
+
 	float ormProgress = 0.0f;
 	std::atomic<bool> needsPreviewUpdate = false;
 	std::atomic<bool> generatingORM = false;
 	std::atomic<bool> loadingTexture;
 	std::string outputUnreal = "orm_unreal.png";
 	std::string outputUnity = "orm_unity.png";
-	
+
 	std::mutex loadingMutex;
 	std::thread loadingThread;
-	
+
 	static constexpr int resolutionValues[6] = { 128, 256, 512, 1024, 2048, 4096 };
 	static constexpr const char* resolutionOptions[6] = { "128","256","512","1024","2048","4096" };
 };
